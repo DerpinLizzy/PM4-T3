fabaf802099aa4eb00c416b4b61426c9607a478e
DerpinLizzy <pazemos@gmail.com>
2024-05-14 22:40:26 +0200

    V2.0.2

--- main.cpp
+++ main.cpp
@@ -17,7 +17,14 @@ bool do_reset_all_once = false;                         // Toggled via user butt
 bool positioned = false;
 bool jail = false;
 int vial_count = 8;
-float x_pos = 0;
+float x_pos = 430;
+
+// Check Variables ==================================================
+constexpr float weight_soll = 13.87;
+constexpr float weight_low_thresh = 13.4;
+constexpr float weight_high_thresh = 14.8;
+constexpr float weight_opened_low = 11.5;
+constexpr float weight_opened_high = 13.3;
 
 // Load Cell Variables ==============================================
 constexpr float loadCellMaxWeight_kg = 0.1;                 // Maximum tolerable Weight on Load Cell
@@ -32,17 +39,19 @@ float loadCellVoltage_mv = 0;       // Adjusted readout of gauge (read - zero va
 float weight_g = 0;                 // Conversion results in kg
 float amplifierOffset = 0;          // Zero value
 
-// Check Variables ==================================================
-constexpr float weight_soll = 13.87;
-constexpr float weight_low_thresh = 13.4;
-constexpr float weight_high_thresh = 14.8;
-constexpr float weight_no_cap_low = 11.5;
-constexpr float weight_no_cap_high = 13.3;
-
 // Motor Variables ==================================================
 constexpr float sir_cumfrence = M_PI * GEAR_DIAMETER;
 constexpr float one_turn = 0.165;
 constexpr float l_factor = sir_cumfrence/one_turn;
+
+constexpr float max_voltage = 6.0f;                    // define maximum voltage of battery packs, adjust this to 6.0f V if you only use one batterypack
+constexpr float counts_per_turn_A = 48.0f*195.3;       // define counts per turn at gearbox end: counts/turn * gearratio
+constexpr float counts_per_turn_B = 48.0f *  390.6;
+constexpr float kn_A = 73.0f / 6.0f;                   // define motor constant in RPM/V
+constexpr float kn_B = 36.0f / 6.0f;  
+constexpr float k_gear_A = 195.3f / 78.125f;           // define additional ratio in case you are using a dc motor with a different gear box, e.g. 100:1
+constexpr float k_gear_B =  390.6f / 78.125f;
+constexpr float kp = 0.2f;
 
 // Output Variables =================================================
 char faults[9] = {'0'};
@@ -50,38 +59,32 @@ static int error_count = 0;
 
 // Global Objects ===================================================
 /* PinMap:
-PB_10 - SPI2_SCK pin used for load cell SCK
-PB_4  - SPI1_MISO pin used for load cell data transfer from amplifier to controller
-PB_8  - I2C SCL pin used for communication with arduino
-PB_9  - I2C SDA pin used for communication with arduino
 BUTTON1 - on-board user button blue
 LED1  - on-board LED 1
 PA_7  - DigitalIn for limit switch
 
-PC_2  - AnalgOut for tube holder servo (A0)
+PB_10 - SPI2_SCK pin used for load cell SCK
+PB_4  - SPI1_MISO pin used for load cell data transfer from amplifier to controller
+
+PB_8  - I2C SCL pin used for communication with arduino
+PB_9  - I2C SDA pin used for communication with arduino
+
+PC_2  - AnalogOut for tube holder servo (A0)
 PB_2  - DigitalOut for Z-Axis Servo (D0)
-PC_8  - DigitalOut for top clamp servo (D1)
+PC_8  - DigitalOut for clamp servo (D1)
 PB_13 - Feed DC Motor (M1)
-PA_9  - Clamp DC Motor (M2)
+PA_9  - Spin DC Motor (M2)
 PB_15 - DC Motor Ground for both M1 and M2
 PA_6  - Feed DC Motor Encoder Signal A
 PC_7  - Feed DC Motor Encoder Signal B
-PB_6  - Clamp DC Motor Encoder Signal A
-PB_7  - Clamp DC Motor Encoder Signal B
+PB_6  - Spin DC Motor Encoder Signal A
+PB_7  - Spin DC Motor Encoder Signal B
 */
 HX711 gauge(amplifierVoltage_V,PB_10,PB_4,amplifierGain);
-DebounceIn user_button(PC_13);                          // create InterruptIn interface object for user button
+DebounceIn user_button(BUTTON1);                          // create InterruptIn interface object for user button
 DebounceIn limit_switch(PA_7, PullUp);
 DigitalOut user_led(LED1);
 I2C com(I2C_SDA,I2C_SCL);
-
-// Motor Objects ====================================================
-// Servos
-
-
-// DC
-
-
 
 // Function Prototypes ==============================================
 void user_button_pressed();                         // custom functions which gets executed when user button gets pressed
@@ -96,25 +99,25 @@ void move_rack(float length, PositionController & positionController_A);
 int main(){
 
     // Interrput - Setup ============================================
+    Timer main_task_timer;
     user_button.fall(&user_button_pressed);
     limit_switch.rise(&limit_switch_event);
-    Timer main_task_timer;
-   
+
     // Load Cell - Setup ============================================
     gauge.powerUp();
     gauge.setGain();
     gauge.setDrift(amplifierDrift_V);
 
     while(gauge.isReady() == true){ }
+    thread_sleep_for(10);
     amplifierOffset = gauge.read();
-    printf("\nOffset = %f",amplifierOffset);
-    printf("\nloadcell");
+    printf("\nOffset = %f\n",amplifierOffset);
+    printf("loadcell\n");
 
-    // Motor - Setup: Servos =========================================
-    Servo servo_S4(PC_8);   //Servo orange
-    Servo servo_S1(PB_2);     // Big Servo
-    Servo servo_S2(PC_3);     // Servo Cap black
-
+    // Motor Setup: Servos [1 = Z-Axis // 2 = hold // 4 = crab Clamp]
+    Servo servo_S1(PB_2);       // Big Servo - Z-Axis
+    Servo servo_S2(PC_3);       // Servo Cap black - stick hold 
+    Servo servo_S4(PC_8);       // Servo orange - crab clamp
     servo_S1.setPeriod_mus(20000);
     servo_S2.setPeriod_mus(20000);
     servo_S4.setPeriod_mus(20000);
@@ -122,36 +125,24 @@ int main(){
     float servo_S2_angle = 0;
     float servo_S4_angle = 0.0f;
 
-        //Servo1 start position
-        servo_S1.enable();
-        servo_S1.setNormalisedAngle(0.03f);
-        thread_sleep_for(3000);
-        servo_S1.disable();
-        thread_sleep_for(3000);
-            
-        // Claw start position
-        servo_S4.enable();
-        servo_S4.setNormalisedAngle(0.12f);
-        thread_sleep_for(2000);
-        servo_S4.disable();
-        thread_sleep_for(3000);
+    //Servo1 start position
+    servo_S1.enable();
+    servo_S1.setNormalisedAngle(0.03f);
+    thread_sleep_for(1000);
+    servo_S1.disable();
 
-    printf("\nservo init done");
+    // Claw start position
+    servo_S4.enable();
+    servo_S4.setNormalisedAngle(0.12f);
+    thread_sleep_for(1000);
+    servo_S4.disable();
+    printf("servo init done\n");
 
-    // Motor - Setup: DC [ A = Feed // B = Spin ]======================
-    constexpr float max_voltage = 6.0f;                    // define maximum voltage of battery packs, adjust this to 6.0f V if you only use one batterypack
-    constexpr float counts_per_turn_A = 48.0f*195.3;       // define counts per turn at gearbox end: counts/turn * gearratio
-    constexpr float counts_per_turn_B = 48.0f *  390.6;
-    constexpr float kn_A = 73.0f / 6.0f;                   // define motor constant in RPM/V
-    constexpr float kn_B = 36.0f / 6.0f;  
-    constexpr float k_gear_A = 195.3f / 78.125f;           // define additional ratio in case you are using a dc motor with a different gear box, e.g. 100:1
-    constexpr float k_gear_B =  390.6f / 78.125f;
-    constexpr float kp = 0.2f;
-
+    // Motor Setup: DC [ A = Feed // B = Spin ]======================
     DigitalOut enable_motors (PB_15);                   // M-
-    FastPWM pwm_A(PB_13);                               // M+
+    FastPWM pwm_A(PB_13);                               // A M+
     EncoderCounter encoder_A(PA_6, PC_7);               // Encoder Pins A
-    FastPWM pwm_B(PA_10);                               // M+
+    FastPWM pwm_B(PA_10);                               // B M+
     EncoderCounter encoder_B(PA_0, PA_1);               // Encoder Pins B
     enable_motors = 1;
 
@@ -164,7 +155,7 @@ int main(){
     positionController_B.setSpeedCntrlGain(kp * k_gear_B);   // adjust internal speed controller gain, this is just an example
     float max_speed_rps_B = 0.5f; // define maximum speed that the position controller is changig the speed, has to be smaller or equal to kn * max_voltage
     positionController_B.setMaxVelocityRPS(max_speed_rps_B);
-    printf("\ndc init done\n");
+    printf("dc init done\n");
 
     // Code =========================================================
     main_task_timer.start();
@@ -177,16 +168,16 @@ int main(){
         if (do_execute_main_task){
             do_execute_main_task = false;
             printf("is on\n");
-            printf("%f\n",x_pos);
+            printf("x_pos = %f\n", x_pos);
 
             // move rack to zeroing position
             while(!positioned){
-                move_rack( 5, positionController_A);
-                thread_sleep_for(50);
+                move_rack(5, positionController_A);
+                thread_sleep_for(10);
             }
             thread_sleep_for(1000);
 
-            // feed dc motor: move to first position weighing
+            // feed motor: move to first position weighing
             move_rack(-25, positionController_A);
             thread_sleep_for(1000);
 
@@ -196,81 +187,88 @@ int main(){
                 if(weight_g > weight_low_thresh && weight_g < weight_high_thresh){
                     
                     move_rack(15, positionController_A);     // clamp pos
-                    thread_sleep_for(5000);
+                    thread_sleep_for(500);
 
-                    //Servo1 down
+                    // Servo1 down
                     servo_S1.enable();
                     servo_S1.setNormalisedAngle(0.05f);
-                    thread_sleep_for(2000);
+                    thread_sleep_for(1000);
                     servo_S1.disable();
-                    thread_sleep_for(3000);
+                    thread_sleep_for(50);
 
-                    //Servo2 hold
+                    // Servo2 hold
                     servo_S2.enable();
                     servo_S2.setNormalisedAngle(0.2f);
-                    thread_sleep_for(3000);
+                    thread_sleep_for(50);
 
-                    //Clamp
+                    // Clamp
                     servo_S4.enable();
                     servo_S4.setNormalisedAngle(0.08);
+                    thread_sleep_for(1000);
+
+                    // Twist Cap
+                    float angle_tracker_b = positionController_B.getRotation();
+                    positionController_B.setDesiredRotation(angle_tracker_b-0.06f);
                     thread_sleep_for(3000);
 
-                    //Twist Cap
-                    positionController_B.setDesiredRotation(-0.06f);
-                    printf("g: %f\n", positionController_B.getRotation());
-                    thread_sleep_for(5000);
-
-                     //Servo1 up
+                    // Servo1 up
                     servo_S1.enable();
                     servo_S1.setNormalisedAngle(0.03f);
-                    thread_sleep_for(3000);
+                    thread_sleep_for(1000);
                     servo_S1.disable();
-                    thread_sleep_for(3000);
+                    thread_sleep_for(50);
 
-                    move_rack(25, positionController_A);      // deposite pos
+                    move_rack(-25, positionController_A);      // deposite pos
+                    thread_sleep_for(500);
 
                     //Servo2 release
                     servo_S2.setNormalisedAngle(0.12);
+                    thread_sleep_for(500);
                     servo_S2.disable();
-                    thread_sleep_for(3000);
+                    thread_sleep_for(50);
 
                     //Untwist Cap
                     positionController_B.setDesiredRotation(0.03f);
-                    printf("g: %f\n", positionController_B.getRotation());
-                    thread_sleep_for(5000);
+                    thread_sleep_for(3000);
 
-                    move_rack(-10, positionController_A);     // measure pos
+                    move_rack(10, positionController_A);     // measure pos
+                    thread_sleep_for(500);
 
                     measure_weight();
 
-                    move_rack(25, positionController_A);
+                    move_rack(-25, positionController_A);
+                    thread_sleep_for(500);
 
-                    if(weight_g < weight_no_cap_low || weight_g > weight_no_cap_high){
+                    if(weight_g < weight_opened_low || weight_g > weight_opened_high){
                         mark_error(i, faults);
                     }
 
                 }else{
                     mark_error(i, faults);
-                    move_rack(50, positionController_A);
+                    move_rack(-50, positionController_A);
+                    thread_sleep_for(1000);
                 }
             }
-                    //Servo1 start position
-        servo_S1.enable();
-        servo_S1.setNormalisedAngle(0.03f);
-        thread_sleep_for(3000);
-        servo_S1.disable();
-        thread_sleep_for(3000);
-            
-        // Claw start position
-        servo_S4.enable();
-        servo_S4.setNormalisedAngle(0.12f);
-        thread_sleep_for(2000);
-        servo_S4.disable();
-        thread_sleep_for(3000);
+            //Servo1 start position
+            servo_S1.enable();
+            servo_S1.setNormalisedAngle(0.03f);
+            thread_sleep_for(1000);
+            servo_S1.disable();
+            thread_sleep_for(50);
+                
+            // Claw start position
+            servo_S4.enable();
+            servo_S4.setNormalisedAngle(0.12f);
+            thread_sleep_for(1000);
+            servo_S4.disable();
+            thread_sleep_for(50);
 
+            // write your faults to display
             com.write(SDA_ADDR,faults,error_count+1);
+            
             // eject rack
             move_rack(45, positionController_A);
+            thread_sleep_for(500);
 
         // End if do_execute_main_task
         }else{
@@ -300,14 +298,15 @@ void measure_weight(void){
     weight_g *= measurementInaccuracy;
 
     // printf("\nLoadcell Readout: %f",readout);
-    // printf("\nAdjusted Readout: %f", loadCellVoltage_mv);
+    // printf("\nAdjusted Readout: %f",loadCellVoltage_mv);
     printf("\nCurrent weight in grams: %f\n", weight_g);
+    thread_sleep_for(1000);
 }
 
 void move_rack(float length, PositionController & positionController_A){
-    float angle_tracker = 0;
-    angle_tracker = positionController_A.getRotation();
+    x_pos += length;
     float revolutions = length/l_factor;
+    float angle_tracker = positionController_A.getRotation();
     positionController_A.setDesiredRotation(angle_tracker + revolutions);
     thread_sleep_for(100);
 }
@@ -329,7 +328,7 @@ float voltToKilograms(float measuredVoltage, float maxLoad, float excitingVolt, 
 
 void limit_switch_event(){
     positioned = true;
-    x_pos = 100;
+    x_pos = 0;
 }
 
 void user_button_pressed(){
